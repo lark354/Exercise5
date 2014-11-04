cd9435d2ff08626ce8157dda2144329fc60918af
Luigi Moca <lmoca@purdue.edu>
2014-11-04 15:11:40 -0500

    Added screenshots, code is documented, added a flowchart of the program, and the code is also included.

---
+++ Doxygen Doc/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['exercise5_2ec',['exercise5.c',['../exercise5_8c.html',1,'']]]
+];
---
+++ exercise5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* This is a comment. */
+int main()
+{
+    int distance = 100;
+
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+	getchar();
+	printf("I understand and get the syntax for most of the operations here\n"); 
+    return 0;
+}
